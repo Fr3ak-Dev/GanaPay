5f4e1fdf81ebc1005877f268148abfe82f9803d5
gustavo <kg.2k.pl@gmail.com>
2026-02-17 23:13:32 -0400

    feat(account): add mappings for ResumenCuentaDTO and CrearCuentaDTO

--- Backend/GanaPay.Application/Mappings/AutoMapperProfile.cs
+++ Backend/GanaPay.Application/Mappings/AutoMapperProfile.cs
@@ -3,6 +3,7 @@ using GanaPay.Application.DTOs.Auth;
 using GanaPay.Application.DTOs.Cuentas;
 using GanaPay.Application.DTOs.Transacciones;
 using GanaPay.Core.Entities;
+using GanaPay.Core.Enums;
 
 namespace GanaPay.Application.Mappings;
 
@@ -11,7 +12,7 @@ public class AutoMapperProfile : Profile
     public AutoMapperProfile()
     {
         // ==================== MAPEOS DE USUARIO ====================
-        
+
         // Mapeo simple
         CreateMap<Usuario, UsuarioDTO>();
         // Mapeo con colección anidada
@@ -25,20 +26,20 @@ public class AutoMapperProfile : Profile
             .ForMember(dest => dest.Activo, opt => opt.Ignore()) // Se asigna manualmente (true)
             .ForMember(dest => dest.FechaRegistro, opt => opt.Ignore()) // Se asigna manualmente (DateTime.UtcNow)
             .ForMember(dest => dest.Cuentas, opt => opt.Ignore()); // No se crean cuentas en registro
-        
+
         // ==================== MAPEOS DE CUENTA ====================
 
         // Mapeo condicional con navigation properties
         CreateMap<Cuenta, CuentaDTO>()
             .ForMember(dest => dest.NombreUsuario, opt => opt.MapFrom(src => src.Usuario != null ? src.Usuario.NombreCompleto : null));
-        
+
         // ==================== MAPEOS DE TRANSACCION ====================
-        
+
         // Mapeo de Transaccion con 2 navigation properties
         CreateMap<Transaccion, TransaccionDTO>()
             .ForMember(dest => dest.NumeroCuentaOrigen, opt => opt.MapFrom(src => src.CuentaOrigen != null ? src.CuentaOrigen.NumeroCuenta : null))
             .ForMember(dest => dest.NumeroCuentaDestino, opt => opt.MapFrom(src => src.CuentaDestino != null ? src.CuentaDestino.NumeroCuenta : null));
-        
+
         CreateMap<TransferenciaRequestDTO, Transaccion>()   // Para crear transacción
             .ForMember(dest => dest.Id, opt => opt.Ignore()) // Auto-generado
             .ForMember(dest => dest.TipoTransaccion, opt => opt.Ignore()) // Se asigna manualmente
@@ -49,5 +50,29 @@ public class AutoMapperProfile : Profile
             .ForMember(dest => dest.CuentaDestino, opt => opt.Ignore()) // Navigation property
             .ForMember(dest => dest.ReferenciaExterna, opt => opt.Ignore())
             .ForMember(dest => dest.CodigoQR, opt => opt.Ignore());
+
+        // ==================== NUEVOS MAPEOS DE CUENTA ====================
+
+        CreateMap<Cuenta, ResumenCuentaDTO>()
+            .ForMember(dest => dest.NombreUsuario,
+                opt => opt.MapFrom(src =>
+                    src.Usuario != null ? src.Usuario.NombreCompleto : null))
+            .ForMember(dest => dest.TipoMonedaDescripcion,
+                opt => opt.MapFrom(src =>
+                    src.TipoMoneda == TipoMoneda.Bolivianos ? "Bolivianos (Bs.)" : "Dólares ($)"))
+            .ForMember(dest => dest.TotalTransacciones,
+                opt => opt.MapFrom(src =>
+                    src.TransaccionesOrigen.Count + src.TransaccionesDestino.Count));
+
+        CreateMap<CrearCuentaDTO, Cuenta>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.NumeroCuenta, opt => opt.Ignore())
+            .ForMember(dest => dest.Activa, opt => opt.Ignore())
+            .ForMember(dest => dest.FechaCreacion, opt => opt.Ignore())
+            .ForMember(dest => dest.Usuario, opt => opt.Ignore())
+            .ForMember(dest => dest.TransaccionesOrigen, opt => opt.Ignore())
+            .ForMember(dest => dest.TransaccionesDestino, opt => opt.Ignore())
+            .ForMember(dest => dest.Saldo,
+                opt => opt.MapFrom(src => src.SaldoInicial));
     }
 }
